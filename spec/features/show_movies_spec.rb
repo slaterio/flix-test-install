3b521793d9a153893cd1340762c50c5f26aae14f
Ben Slater <ben@overmation.com>
2016-02-11 16:31:06 +0000

    added new test to navigate between pages. created new spec/support/attributes.rb file to contain common methods

--- spec/features/show_movies_spec.rb
+++ spec/features/show_movies_spec.rb
@@ -4,11 +4,7 @@ describe "Viewing the movie details" do
 
   it "shows the movies details" do
 
-  movie = Movie.create(title: "Iron Man",
-                      rating: "PG-13",
-                      total_gross: 318412101.00,
-                      description: "Tony Stark builds an armored suit to fight the throes of evil",
-                      released_on: "2008-05-02")
+  movie = Movie.create(movie_attributes(total_gross: 318412101.00))
 
   visit movie_url(movie)
 
