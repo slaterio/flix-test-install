acb5c927eb724af8d7673eb03fb395b75a769940
Ben Slater <ben@overmation.com>
2016-04-04 12:36:15 +0100

    added review resource. declared model relationships and added verification

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ActiveRecord::Base
 
+  has_many :reviews, dependent: :destroy
+
   validates :title, :released_on, :duration, presence: true
  
   validates :description, length: { minimum:25 }
